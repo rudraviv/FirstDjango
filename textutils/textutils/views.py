da240c11b99f3a053acaa8aa6d34b0991ff62434
rudraviv <rudraviv95@gmail.com>
2020-02-04 00:31:37 +0530

    added some functionality and Bootstrap

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -9,8 +9,15 @@ def analyze(request):
     #Get text
     DjText=request.GET.get('text','default')
     print(DjText)
+
+    #for checkbox
     removePunc=request.GET.get('removepunc','default')
     print(removePunc)
+    fullcaps=request.GET.get('fullcaps','default')
+    newlinerem=request.GET.get('newlineremover','default')
+    etcSpaceRemoved=request.GET.get('extraspaceremover','default')
+    CountChar=request.GET.get('charactercount','default')
+    #Logic for analyze text
     if removePunc=="on":
         punctuations='''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
         analyzed= ""
@@ -19,11 +26,53 @@ def analyze(request):
                 analyzed=analyzed+char
         print(analyzed)
         params={'purpose':'Removed Punctuations','analyzed_text':analyzed}
+
         #Analyze the text
+        return render(request,'analyze.html',params)
+    elif fullcaps=="on":
+        analyzed=""
+        for char in DjText:
+            analyzed=analyzed+char.upper()
+            params={'purpose':'Change to UpperCase','analyzed_text':analyzed}
+        #Analyze the text
+        return render(request,'analyze.html',params)
+    elif  newlinerem=="on":
+        analyzed=""
+        for char in DjText:
+            if char!="\n":
+                analyzed=analyzed+char
+        params={'purpose':'NewLines Removed','analyzed_text':analyzed}
+         #Analyze the text
+        return render(request,'analyze.html',params)
+
+    elif  etcSpaceRemoved=="on":
+        analyzed=""
+        for index,char in enumerate(DjText):
+            if DjText[index]==" "and DjText[index+1]==" ":
+                pass
+            else:
+                analyzed=analyzed+char
+            params={'purpose':'Extra Space Removed','analyzed_text':analyzed}
+         #Analyze the text
+        return render(request,'analyze.html',params)
+
+    elif  CountChar=="on":
+        total=0
+        analyzed="Total Characters: "
+        for char in DjText:
+            if char==" ":
+                pass
+            else:
+                total+=1
+        total=str(total)
+        analyzed+=total
+        params={'purpose':'Character Counting','analyzed_text':analyzed}
+         #Analyze the text
         return render(request,'analyze.html',params)
     else:
         return HttpResponse("<h1>ERROR</h1>")
 
+    
 
 '''def capfirst(request):
    return HttpResponse("Capitalize First")
